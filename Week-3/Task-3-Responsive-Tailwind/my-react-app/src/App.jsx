28b81ddb48baee98c49abc6fb1f53f6eeada08f8
idkzeynav <zeynabxco@gmail.com>
2024-06-30 21:17:45 +0500

    week 3 tasks

---
+++ Week-3/Task-3-Responsive-Tailwind/my-react-app/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+const App =() =>{
+  return <section className="grid gap-8 md:grid-cols-2 md:items-center md:text-left sm:max-xl:bg-blue-50 sm:max-xl:p-9 3xl:">
+
+    <div>
+      <img src="https://i.pinimg.com/564x/9a/20/56/9a205601fbc5ea4a108287a5de284f27.jpg" alt="" className="w-full rounded-lg"/>
+    </div>
+    <div>
+      <h1 className="text-4xl font-meduim mb-2">Mountain</h1>
+      <p>
+        
+A veil of mist clung stubbornly to the mountain's peak, occasionally swirling to reveal jagged, snow-capped rock. Below, a carpet of wildflowers, vibrant purples and yellows, defied the harshness of the terrain. A lone marmot, perched on a boulder, let out a sharp whistle, its tiny silhouette stark against the vast canvas of the sky.
+      </p>
+    </div>
+  </section>;
+};
+
+export default App;
